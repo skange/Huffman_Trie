379370620ba13719f27c2b13319091c2daddb972
keaidema <17303641307@163.com>
2018-08-09 19:42:27 +0800

    build

--- build.c
+++ build.c
@@ -5,45 +5,45 @@
 	> Created Time: 2018年08月09日 星期四 14时40分25秒
  ************************************************************************/
 
-#include <stdio.h>
-#include "hf_trie.h"
+#include "HF_Trie.h"
 
-HFNode **build(int word_cnt) {
-    for (int times = 0; times < word_cnt - 1; times++) {
+void build(HFNode **arr) {
+    for (int times = 0; times < BASE - 1; times++) {
         HFNode *minNode = arr[0];
         int ind = 0;
-        for (int i = 1; i < n - times; i++) {
+        for (int i = 1; i < BASE - times; i++) {
             if (arr[i]->freq >= minNode->freq) continre;
             minNode = arr[i];
             ind = i;
         }
-        swap(arr[ind], arr[n - times - 1]);
+        swap(arr[ind], arr[BASE - times - 1]);
         minNode = arr[0];
         ind = 0;
-        for (int i = 1; i < n - times - 1; i++) {
+        for (int i = 1; i < BASE - times - 1; i++) {
             if (arr[i]->freq >= minNode->freq) continre;
             minNode = arr[i];
             ind = i;
         }
-        swap(arr[ind], arr[n - times - 2]);
-        HFNode *new_node = get_hfnode();
-        new_node->lchild = arr[n - times - 1];
-        new_node->rchild = arr[n - times - 2];
-        new_node->freq = arr[n - times - 1]->freq + arr[n - times - 2]->freq;
-        arr[n - times - 2] = new_node;
+        swap(arr[ind], arr[BASE - times - 2]);
+        HFNode *new_node = get_hfNode();
+        new_node->lchild = arr[BASE - times - 1];
+        new_node->rchild = arr[BASE - times - 2];
+        new_node->freq = arr[BASE - times - 1]->freq + arr[BASE - times - 2]->freq;
+        arr[BASE - times - 2] = get_hfNode();
     }
     return ;
 }
 
 void extract(HFNode* root, unsigned char *buff, int n) {
-    buff[n] = '\0';
     if (root->lchild == NULL && root->rchild == NULL) {
-        //strcpy(hftable[root->ch], buff);
+        for (int i = 0; i < 8; i++) {
+            hftable[root][i] = buff[i];
+        }
         return ;
     }
     buff[n] = '0';
-    extract(root->lchild, buff, );
+    extract(root->lchild, buff, n + 1);
     buff[n] = '1';
-    extract(root->rchild, buff, );
+    extract(root->rchild, buff, n + 1);
 }
 
