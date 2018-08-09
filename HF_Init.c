586e64b0da50621d436d02cd63db3ebc48c0cef3
keaidema <17303641307@163.com>
2018-08-09 23:23:38 +0800

    根据字典统计得到词频并建立对应的哈弗曼树，导出哈弗曼表

--- HF_Init.c
+++ HF_Init.c
@@ -5,20 +5,66 @@
 	> Created Time: 2018年08月09日 星期四 20时03分34秒
  ************************************************************************/
 
-#include "hf_trie.h"
+#include "HF_Trie.h"
+
+void build(HFNode **arr) {
+    for (int times = 0; times < BASE - 1; times++) {
+        HFNode *minNode = arr[0];
+        int ind = 0;
+        for (int i = 1; i < BASE - times; i++) {
+            if (arr[i]->freq >= minNode->freq) continue;
+            minNode = arr[i];
+            ind = i;
+        }
+        swap(arr[ind], arr[BASE - times - 1]);
+        minNode = arr[0];
+        ind = 0;
+        for (int i = 1; i < BASE - times - 1; i++) {
+            if (arr[i]->freq >= minNode->freq) continue;
+            minNode = arr[i];
+            ind = i;
+        }
+        swap(arr[ind], arr[BASE - times - 2]);
+        HFNode *new_node = get_hfNode();
+        new_node->lchild = arr[BASE - times - 1];
+        new_node->rchild = arr[BASE - times - 2];
+        new_node->freq = arr[BASE - times - 1]->freq + arr[BASE - times - 2]->freq;
+        arr[BASE - times - 2] = get_hfNode();
+    }
+    return ;
+}
+
+int *get_word_freq() {
+    int *word_freq = (int*)calloc(sizeof(int), 10000);
+    FILE *fp = NULL;
+    fp = fopen("./corpus", "r");
+    if (fp == NULL) perror("fopen:");
+    while (!feof(fp)) {
+        char str[1000];
+        memset(str, 0, 1000);
+        fgets(str, 100, fp);
+        for (int i = 0; str[i]; i++) {
+            word_freq[(int)str[i]]++;    
+        }
+    }
+    return word_freq;
+}
+
+void extract(HFNode *root, unsigned char *buff, int n) {
+    
+}
 
 void hf_init() {
-    int *freq_ = word_freq();
+    int *word_freq = get_word_freq();
     HFNode *freq_arr[BASE];
     int count = 0;
     for (int i = 0; i < BASE; i++) {
-        if (freq_arr[i] == 0) freq_arr[i] = 1;
         HFNode *new_node = get_hfNode();
         new_node->ch = i;
-        new_node->freq = freq_arr[i];
+        new_node->freq = word_freq[i];
         freq_arr[count++] = new_node;
     }
-    build(hf_arr, BASE);
+    build(freq_arr);
     char buff[1000] = {0};
-    extract(freq_arr[0], buff, int n);
+    //extract(freq_arr, buff, int n);
 }
