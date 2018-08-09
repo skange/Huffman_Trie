b60afaa6af2a4ef7de4fd4649387388da7ffe9b7
skange <840408402@qq.com>
2018-08-09 15:47:38 +0800

    结构定义及函数声明

---
+++ HF_Trie.h
@@ -0,0 +1,44 @@
+/*************************************************************************
+	> File Name: hf_trie.h
+	> Author: ShangKe
+	> Mail: 
+	> Created Time: 2018年08月09日 星期四 11时51分33秒
+ ************************************************************************/
+
+#ifndef _HF_TRIE_H
+#define _HF_TRIE_H
+
+
+typedef struct HFNode {
+    int ch;
+    int freq;
+    struct HFNode *lchild, *rchild;
+} HFNode;
+
+typedef struct Node {
+    int flag;
+    struct Node *next[2];//l = 0, r = 1;
+} Node, *Trie;
+
+typedef struct pattern_table {
+    char *pattern;
+    char *hfcode;
+} PatNode;
+
+unsigned char hftable[256][10];
+int node_cot = 0;
+
+//HF
+HFNode *get_hfNode();
+void hf_init();//get hftable
+int *word_freq();  //return *freq_arr
+HFNode **build(int word_cnt); //(字节总长度) return hf_arr
+
+//Trie
+Node *get_trie_node();
+PatNode *get_patnode(int);
+Node *insert(Trie, const unsigned char *);//(root, pattern) return root
+void search(Trie, const unsigned char **);//(root, text)
+char *find_word(unsigned char *);//(hfcode)
+void clear(Trie);
+#endif
